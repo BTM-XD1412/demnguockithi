382d324cf236edf5fc53c8fecc79fabc364de709
BTM-XD1412 <thelinh26052009@gmail.com>
2026-09-17 18:56:47 +0700

    fix(constants): Đặt EXAM_TARGET_DATE về mốc 00:00 ngày 11/06/2027 GMT+7

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const EXAM_TARGET_DATE = "2027-06-11T07:30:00+07:00";
+export const EXAM_TARGET_DATE = "2027-06-11T00:00:00+07:00";
 export const EXAM_YEAR = 2027;
 export const EXAM_TITLE = "Kỳ thi tốt nghiệp THPT 2027";
 export const EXAM_DATE_DISPLAY = "11/06/2027";
